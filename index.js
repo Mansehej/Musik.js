163d206ce1a0e759b20224041872d169d3d3aab8
Mansehej <mansehej@gmail.com>
2020-03-24 01:52:05 +0530

    [FEATURE] Accept array of notes as input + Error handling
    - makeMidi( ) can now take the input of an array of notes in it's first argument
    - Implemented error handling

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
-const scribble = require('scribbletune');
+const scribble = require("scribbletune");
 
-async function makeMidi(notes, normalScale=4, outputName='musik') {
-    await  scribble.midi(scribble.clip(makeClip(notes,normalScale)),outputName)
+async function makeMidi(notes, normalScale = 4, outputName = "musik") {
+  try {
+    await scribble.midi(
+      scribble.clip(makeClip(notes, normalScale)),
+      outputName
+    );
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function notesArrayFromString(notes) {
+  return notes.split(" ");
 }
 
 function makeClip(notes, normalScale) {
-    var notesArray = notes.split(' ')
-    var pattern = ''
-    var spliceLocations = []
-    for(var i = 0; i < notesArray.length; i++) {
-        if(notesArray[i] == '_') {
-            pattern = pattern + '_'
-            spliceLocations.push[i]
-            notesArray.splice(i,1)
-            i--
-        }
-        else {
-            pattern = pattern + 'x'
-            if(!/\d/.test(notesArray[i])) {
-                notesArray[i] = notesArray[i] + normalScale
-            }
-        }
+  let notesArray;
+
+  // Check if input type is valid
+
+  if (typeof notes == "string") {
+    // Convert string of notes into array
+    notesArray = notesArrayFromString(notes);
+  } else if (Array.isArray(notes)) {
+    notesArray = notes;
+  } else {
+    throw new TypeError("Notes can only be of type string or array.");
+  }
+
+  var pattern = "";
+  for (var i = 0; i < notesArray.length; i++) {
+
+    // Handle for empty note ( _ )
+    if (notesArray[i] == "_") {
+      pattern = pattern + "_";
+      notesArray.splice(i, 1);
+      i--;
+    } else {
+      pattern = pattern + "x";
+      if (!/\d/.test(notesArray[i])) {
+        notesArray[i] = notesArray[i] + normalScale;
+      }
     }
-    var musicObj = {
-        pattern: pattern,
-        notes: notesArray
-    }
-    return musicObj
+  }
+  var musicObj = {
+    pattern: pattern,
+    notes: notesArray
+  };
+  return musicObj;
 }
 
 module.exports = {
-    makeMidi
-}
+  makeMidi
+};
